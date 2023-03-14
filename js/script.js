b467c44de894829c59d918a34447f38bc83cb6ab
Evgeniy Salish <jeka5650962@gmail.com>
2023-03-14 13:49:28 +0300

    140323 1349

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ new Swiper('.block__items', {
     navigation: {
         nextEl: '.navigation__next',
         prevEl: '.navigation__previous'
-    }
+    },
+    simulateTouch: false,
+    slidesPerView: 1
 })
